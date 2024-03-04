4a97bf0816689e01ad1a56562c13b25432a98e25
Jairo Sousa <58406697+JairoSSantos@users.noreply.github.com>
2024-03-04 01:32:25 -0300

    Anninos

--- modules/numeric.py
+++ modules/numeric.py
@@ -1,10 +1,11 @@
 import numpy as np
-from sympy import Matrix
-from math import factorial, sqrt
+import sympy as sp
+from math import factorial
 from scipy.ndimage import convolve1d
 from dataclasses import dataclass
 from functools import partial
 from typing import Callable, Any
+from abc import ABC, abstractmethod
 
 def diff_coeffs(m: int, offsets: tuple[int], h: float=1) -> np.ndarray:
     '''
@@ -31,48 +32,15 @@ def diff_coeffs(m: int, offsets: tuple[int], h: float=1) -> np.ndarray:
         Finite difference coefficients.
     '''
     offsets = np.r_[offsets]
-    A = Matrix(offsets[:, np.newaxis]**np.arange(len(offsets)))
+    A = sp.Matrix(offsets[:, np.newaxis]**np.arange(len(offsets)))
     return factorial(m)*np.r_[A.inv()][m].astype(float)/h**m
-
-# def diff_matrix(m: int, n: int, order: int, h: float) -> np.ndarray:
-#     '''
-#     Discrete derivative.
-
-#     Parameters
-#     ----------
-#     m: int
-#         Derivative order.
-#     n: int
-#         Amount of the mesh nodes.
-#     order: int
-#         Error order of the approximation.
-#     h: float
-#         Space between the mesh nodes.
-    
-#     Returns
-#     ----------
-#     ndarray[float]
-#         Differentiation matrix.
-#     '''
-#     p = order + m
-#     phalf = p//2
-#     offsets = np.arange(p)
-#     central = diff_coeffs(m, offsets - phalf)
-#     forward = diff_coeffs(m, offsets)
-#     backward = diff_coeffs(m, -offsets[::-1])
-#     M = np.zeros((n, n))
-#     for i in range(n):
-#         if i < phalf: M[i, i:i+p] = forward
-#         elif i > n-ceil(p/2): M[i, i-p+1:i+1] = backward
-#         else: M[i, i-phalf:i+ceil(p/2)] = central
-#     return M/h**m
 
 def argnearest(arr: np.ndarray, value: Any):
     return np.abs(arr - value).argmin()
 
 def rk4_solve(F: Callable[[float, float|np.ndarray], float|np.ndarray], y0: float|np.ndarray, dt: float, t_final: float, t0: float=0, 
-               callback: Callable[[float, float|np.ndarray], float|np.ndarray]=(lambda t, y: y),
-               stop_conditions: tuple[Callable[[float, float|np.ndarray], float|np.ndarray]]=[]):
+               callback: Callable[[float, float|np.ndarray], float|np.ndarray]=None,
+               stop_condition: Callable[[float, float|np.ndarray], float|np.ndarray]=None):
     '''
     Runge-Kutta integration for a generalized ODEs system.
 
@@ -99,24 +67,30 @@ def rk4_solve(F: Callable[[float, float|np.ndarray], float|np.ndarray], y0: floa
     '''
     Y = [y0]
     T = np.arange(t0, t_final, dt)
-    for t in T:
+    for i, t in enumerate(T[1:]):
         y = Y[-1]
-
-        stop = False
-        for func in stop_conditions: 
-            if func(t, Y[-1]):
-                T = np.arange(t0, t, dt)
-                stop = True
-                break
-        if stop: break
 
         k1 = F(t, y)
         k2 = F(t + dt/2, y + k1*dt/2)
         k3 = F(t + dt/2, y + k2*dt/2)
         k4 = F(t + dt, y + k3*dt)
-        Y.append(callback(t, y + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)))
+        Y.append(y + (dt/6)*(k1 + 2*k2 + 2*k3 + k4))
 
-    return T, np.r_[Y]
+        if callback != None: callback(t, Y)
+
+        if stop_condition != None and stop_condition(t, Y[-1]): break
+
+    return T[:i+2], np.r_[Y]
+
+def delta(lamb: float|int) -> float:
+    return np.sqrt(2/lamb)
+
+def kink(x0: float|int, v: float|int, lamb: float|int):
+    gamma = 1/np.sqrt(1 - v**2)
+    c1 = gamma/delta(lamb)
+    x, t =sp.symbols('x, t')
+    z = c1*(x - x0 - v*t)
+    return sp.tanh(z)
 
 class Lattice:
     '''
@@ -218,7 +192,7 @@ class Kink:
     def z(self, x: float|np.ndarray, t: float) -> float|np.ndarray:
         return self._const*(x - self.x0 - self.v*t)
     
-    def __call__(self, x: float|np.ndarray, t: float) -> float|np.ndarray:
+    def call(self, x: float|np.ndarray, t: float) -> float|np.ndarray:
         return np.tanh(self.z(x, t))
     
     def diff_dt(self, x: float|np.ndarray, t: float) -> float|np.ndarray:
@@ -246,31 +220,36 @@ class KinkCollider:
     diff_dx: ndarray
         A matrix that provides the second derivative on the space axis, given the `x_lattice` informations.
     '''
-    x_range: tuple[float]
+    x_min: float
+    x_max: float
+    Nx: int
     x0s: tuple[float]
     dt: float
+    dx: int=None
     order: int=4
 
     def __post_init__(self):
-        self.x = np.arange(*self.x_range)
-        p = self.order + 2
-        self.D2x = diff_coeffs(2, np.arange(p) - p//2, h=self.x_range[-1])
+        self.x = np.linspace(self.x_min, self.x_max, self.Nx)
+        if self.dx == None: self.dx = (self.x_max - self.x_min)/(self.N - 1)
+        p = self.order + 1 # in general p = order + 2, but for central differences p = order + 1
+        self._j = p//2
+        self.D2x = diff_coeffs(2, np.arange(p) - self._j, h=self.dx)
     
-    def F(self, t: float, Y: np.ndarray, lamb: float) -> np.ndarray:
+    def get_system(self, lamb: float) -> np.ndarray:
         '''
-        ODEs system 
-        $$
-            \left\{\begin{array}{ l }
-            \ddot{\Phi } =\Phi ''+\lambda \Phi \left( \Phi ^{2} -1\right)\\
-            d\Phi =\dot{\Phi }  dt
-            \end{array}\right..
-        $$
+        ODEs system.
         '''
-        y, dy_dt = Y
-        return np.stack((
-            dy_dt, # = dy(t)
-            convolve1d(y, self.D2x, mode='reflect') + lamb*y*(1 - y**2) # = ddy(t)
-        ))
+        def F(t: float, Y: np.ndarray):
+            y, dy_dt = Y
+
+            # y[0], y[-1] = y1, y2
+            y_reflected = np.r_[y[1:self._j+1][::-1], y, y[-self._j-1:-1][::-1]]
+
+            return np.stack((
+                dy_dt, # = dy(t)
+                np.convolve(y_reflected, self.D2x, mode='valid') + lamb*y*(1 - y**2) # = ddy(t)
+            ))
+        return F
     
     def collide(self, vs: tuple[float], lamb: float, t_final: float, gnd: int=-1, **rk4_kwargs) -> np.ndarray:
         '''
@@ -279,7 +258,7 @@ class KinkCollider:
         Parameters
         ----------
         vs: tuple[float]
-            Inivial velocitys of the kinks.
+            Initial velocitys.
         lamb: float
             The $\lambda$ factor.
         t_final: float
@@ -287,35 +266,23 @@ class KinkCollider:
         gnd: int
             Field state at left of the spatial range in the initial instant.
         '''
-        y0 = np.zeros((2, len(self.x)))
-        y0[0].fill(gnd)
-        for i, j in enumerate(np.argsort(self.x0s)):
-            q = gnd*(-1)**(i + 1)
-            k = Kink(self.x0s[j], vs[j], lamb)
-            y0[0] += q*k(self.x, t=0)
-            y0[1] += q*k.diff_dt(self.x, t=0)
+        # y0 = np.zeros((2, len(self.x)))
+        # y0[0].fill(gnd)
+        # for i, j in enumerate(np.argsort(self.x0s)):
+        #     q = gnd*(-1)**(i + 1)
+        #     k = Kink(self.x0s[j], vs[j], lamb)
+        #     y0[0] += q*k(self.x, t=0)
+        #     y0[1] += q*k.diff_dt(self.x, t=0)
+
+        field = kink(self.x0s[0], vs[0], lamb) - kink(self.x0s[1], vs[1], lamb) - 1
+        x, t =sp.symbols('x, t')
+        phi = sp.lambdify(x, field.subs({t: 0}), 'numpy')
+        phi_dt = sp.lambdify(x, field.diff(t).subs({t: 0}), 'numpy')
+        y0 = np.stack((
+            phi(self.x), 
+            phi_dt(self.x)
+        ))
         
-        t, y = rk4_solve(partial(self.F, lamb=lamb), y0, self.dt, t_final, **rk4_kwargs)
+        t, Y = rk4_solve(self.get_system(lamb=lamb), y0, self.dt, t_final, **rk4_kwargs)
 
-        return Lattice(x=self.x, t=t), y
-
-    def neumann_boundary(n: int , y: float|tuple[float], dy: float|tuple[float]=0):
-        if y != None:
-            try: len(y)
-            except TypeError: y = (y, y)
-        if dy != None:
-            try: len(dy)
-            except TypeError: dy = (dy, dy)
-
-        def boundary(t: float, Y: np.ndarray):
-            if dy != None:
-                Y[1, n], Y[1, -n-1] = dy
-            if y != None:
-                Y[0, n], Y[0, -n-1] = y
-            Y[0, -n:] = Y[0, -2*n-1:-n-1][::-1]
-            Y[0, :n] = Y[0, n+1:2*n+1][::-1]
-            return Y
-        return boundary
-    
-    def overflowed(t: float, Y: np.ndarray) -> bool:
-        return np.any(np.isnan(Y))
+        return Lattice(x=self.x, t=t), Y[:, 0], Y[:, 1]
---
+++ modules/tools.py
@@ -0,0 +1,58 @@
+from numpy import ndarray
+
+def memory_economize(t: float, Y: ndarray):
+    if len(Y) > 2:
+        del Y[0]
+    return Y
+
+def catch_cm(cm_index: int, Ycm: list):
+    def wrapped(t, Y):
+        y, _ = Y[-1]
+        Ycm.append(y[cm_index])
+        return Y
+    return wrapped
+
+def composite(*fs):
+    def wrapped(t: float, Y: ndarray):
+        for f in fs:
+            Y = f(t, Y)
+        return Y
+    return wrapped
+
+class Tracker:
+    def __init__(self, x: ndarray, x0: float):
+        self.x = x
+        self.x0 = x0
+        self.trail = []
+        self.is_bion = True
+
+    def __call__(self, t: float, Y: ndarray):
+        y, _ = Y[-1]
+        try:
+            k = self.x[y >= 0].max()
+        except:
+            k = np.nan
+        self.trail.append(k)
+        return Y
+    
+class BouncesCounter:
+    def __init__(self, cm_index: int):
+        self.cm_index = cm_index
+        self.bounces = 0
+        self._last_cm = None
+        self.locs = []
+        self._iter = 0
+
+    def __call__(self, t, Y):
+        y_cm = Y[-1][0, self.cm_index]
+        if self._last_cm != None and y_cm <= 0 and self._last_cm > 0:
+            self.bounces += 1
+            self.locs.append(self._iter)
+        self._last_cm = y_cm
+        self._iter += 1
+        return Y
+    
+    def blocker(self, nmax):
+        def condition(t, Y):
+            return self.bounces >= nmax
+        return condition
