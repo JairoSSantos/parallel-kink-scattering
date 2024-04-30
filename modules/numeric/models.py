48af0d6e1c529e8c1bbb7192c3fa58ed1105c7cb
Jairo Sousa <58406697+JairoSSantos@users.noreply.github.com>
2024-04-30 18:11:36 -0300

    Tests

--- modules/numeric/models.py
+++ modules/numeric/models.py
@@ -4,14 +4,6 @@ from dataclasses import dataclass
 
 NUMERIC = float|np.ndarray[float]
 
-def kink(x: NUMERIC, t: float, v: float, c: float=1) -> NUMERIC:
-    gamma = 1/sqrt(1 - v**2/c**2)
-    return np.tanh(gamma*(x - v*t))
-
-def kink_dt(x: NUMERIC, t: float, v: float, c: float=1) -> NUMERIC:
-    gamma = 1/sqrt(1 - v**2/c**2)
-    return -gamma*v/np.cosh(gamma*(x - v*t))**2
-
 # def delta(lamb: float|int) -> float:
 #     return np.sqrt(2/lamb)
 
@@ -41,11 +33,22 @@ def kink_dt(x: NUMERIC, t: float, v: float, c: float=1) -> NUMERIC:
 
 @dataclass
 class Phi4:
-    scale: float=2
-    vacuum: float=1
+    def __init__(self, scale: float=2, vacuum: float=1):
+        self.scale, self.vacuum = scale, vacuum
+        self.scale_factor = self.vacuum*sqrt(self.scale/2)
 
     def __call__(self, y: NUMERIC) -> NUMERIC:
         return (self.scale/4)*(y**2 - self.vacuum**2)**2
     
     def diff(self, y: NUMERIC) -> NUMERIC:
         return self.scale*y*(y**2 - self.vacuum**2)
+
+    def kink(self, x: NUMERIC, t: float, v: float) -> NUMERIC:
+        gamma = 1/sqrt(1 - v**2)
+        z = gamma*(x - v*t)
+        return self.vacuum*np.tanh(self.scale_factor*z)
+
+    def kink_dt(self, x: NUMERIC, t: float, v: float) -> NUMERIC:
+        gamma = 1/sqrt(1 - v**2)
+        z = gamma*(x - v*t)
+        return -self.vacuum*self.scale_factor*gamma*v/np.cosh(gamma*(x - v*t))**2
